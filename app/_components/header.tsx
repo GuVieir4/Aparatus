7abc67274e8daa1b66a43d68636869142f30f3bc
GuVieir4 <gurednike@gmail.com>
2025-11-16 23:13:50 -0300

    feat: add menu component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,39 +2,34 @@
 
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { LogInIcon, LogOutIcon, MenuIcon } from "lucide-react";
-import { authClient } from "@/lib/auth-client";
+import { MenuIcon } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import SidebarMenu from "./sidebar-menu";
 
 const Header = () => {
-  const { data: session } = authClient.useSession();
-
-  async function handleLogin() {
-    await authClient.signIn.social({
-      provider: "google",
-    });
-  }
-  async function handleLogout() {
-    await authClient.signOut();
-  }
-
   return (
     <header className="flex items-center justify-between bg-white px-5 py-6">
       <Image src="/logo.svg" alt="Aparatus" width={100} height={26.09} />
       <div className="flex items-center gap-2">
-        {session ? (
-          <Button variant="outline" onClick={handleLogout}>
-            <LogOutIcon className="mr-2" />
-            Sair
-          </Button>
-        ) : (
-          <Button variant="outline" onClick={handleLogin}>
-            <LogInIcon className="mr-2" />
-            Entrar
-          </Button>
-        )}
-        <Button variant="outline" size="icon">
-          <MenuIcon />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-[370px] p-0">
+            <SheetHeader className="border-b px-5 py-6 text-left">
+              <SheetTitle className="text-lg font-bold">Menu</SheetTitle>
+            </SheetHeader>
+            <SidebarMenu />
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   );
